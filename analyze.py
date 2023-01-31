862cc4d6bf54b2d27ce687a97847781b88d6ac33
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-01-31 21:06:45 +0530

    optimize code

--- analyze.py
+++ analyze.py
@@ -1,5 +1,4 @@
-import csv, os
-from collections import Counter
+import csv, os, collections
 
 filename = "data.csv"
 
@@ -12,15 +11,9 @@ for line in loaded_dataset:
     for number in line:
         values_only.append(number)
 
-values_only_unique = set(values_only)
+counter = collections.Counter(values_only)
 
-print(len(values_only))    
-print(len(values_only_unique))    
-
-data = {}
-for unique_value in values_only_unique:
-    data[unique_value] = values_only.count(unique_value)
-
+data = dict(counter)
 repeats_sorted = sorted(data.items(), key=lambda x:x[1], reverse=True)
 
 if os.path.isfile("repeats.csv"):
@@ -30,4 +23,3 @@ with open("repeats.csv", "w", encoding="utf-8") as _file:
     for repeat in repeats_sorted:
         _file.write(f"{repeat[0]},{repeat[1]}\n")
         print(repeat)
-            
